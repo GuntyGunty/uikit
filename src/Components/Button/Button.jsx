176edf8e9aa33c987e95fe09b09fbe6c630ae2f3
GuntyGunty <gunter0705@yandex.ru>
2019-08-14 23:22:37 +0300

    .

--- src/Components/Button/Button.jsx
+++ src/Components/Button/Button.jsx
@@ -8,29 +8,55 @@ class Button extends Component {
   }
 
   render() {
-    let className = 'Button ';
-    switch(this.props.theme) {
-      case 'accent' :
-      case 'secondary':
-        className += ' ' +  this.props.theme
-        break;
-      default:
-        className += 'primary'
+    let className = 'Button';
+
+    let theme = {
+      primary: 'primary',
+      secondary: 'secondary',
+      accent: 'accent' 
     }
+
+    let state = {
+      enabled: 'enabled',
+      pcossing: 'processing',
+      disabled: 'disabled'
+    }
+
+    let variants = {
+      filled: 'filled',
+      outlined: 'outlined'
+    }
+
+    // getClassName = (obj, props, className) => {
+    //   for(let key in obj) {
+    //     if(key === props) {
+    //       className += + ' ' + key;
+    //     }
+    //   }
+    //   return className
+    // }
+
+    // getClassName(theme, this.props.theme, className);
+
+    console.log(className);
+
+    // switch(this.props.theme) {
+    //   case 'accent' :
+    //   case 'secondary':
+    //     className += ' ' +  this.props.theme
+    //     break;
+    //   default:
+    //     className += 'primary'
+    // }
       
-    switch(this.props.state) {
-      case 'processing':
-      case 'disabled':
-        className +=  ' ' + this.props.state
-        break;
-      default: className += 'enabled'
-    }
+    // switch(this.props.state) {
+    //   case 'processing':
+    //   case 'disabled':
+    //     className +=  ' ' + this.props.state
+    //     break;
+    //   default: className += 'enabled'
+    // }
 
-    getClassName = (element, classesArray) => {
-      switch(element) {
-
-      }
-    }
 
     return (
       <button onClick={this.onClick} className={className} >
