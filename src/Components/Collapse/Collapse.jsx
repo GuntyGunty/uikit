a1cd678a90aaf312ad4cdcabff020e011da573c8
GuntyGunty <gunter0705@yandex.ru>
2019-08-19 08:53:31 +0300

    .

--- src/Components/Collapse/Collapse.jsx
+++ src/Components/Collapse/Collapse.jsx
@@ -2,12 +2,38 @@ import React from 'react'
 import './Collapse.css'
 
 export default class Collapse extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      isOpened: false,
+      children: this.props.children,
+    }
+  }
+
+  onClick = (e) => {
+    this.setState({
+      isOpened: !this.state.isOpened
+    });
+
+    console.log(this.state.isOpened)
+
+    if(this.state.isOpened) {
+      children: null
+     } else {
+      children: this.props.children
+     }
+
+  }
+
   render() {
+    const { isOpened } = this.state;
+
     return (
       <div className='Collapse'>
-        <span className='Collapse_toggler'>Show more</span>
-        <div class='Collapse_container'>
-          {this.props.children}
+        <span className='Collapse_toggler' onClick={this.onClick}>Show more</span>
+        <div className='Collapse_container'>
+          {this.state.children}
         </div>
       </div>
     )
